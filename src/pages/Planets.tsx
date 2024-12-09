e30a30462a9937a9fb6d092ff151a8a9b94a790d
Fernando Herrera <fernando.herrera85@gmail.com>
2024-12-09 12:50:58 -0500

    Implement planet addition functionality in EditPlanetForm and update Planets component to handle new planet submissions. Added state management for form inputs and modified the form to call onAddPlanet with the new planet data upon submission.

--- src/pages/Planets.tsx
+++ src/pages/Planets.tsx
@@ -15,12 +15,16 @@ const Planets: FC = () => {
     });
   }, []);
 
+  const handleAddPlanet = (planet: Partial<Planet>) => {
+    console.log(planet);
+  };
+
   return (
     <>
       <h4 className="text-2xl font-thin mb-4">Agregar y mantener planetas</h4>
       <hr className="border-gray-300 mb-4" />
       {/* Formulario para agregar un planeta */}
-      <EditPlanetForm />
+      <EditPlanetForm onAddPlanet={handleAddPlanet} />
 
       {/* Lista de planetas Grid*/}
       {isLoading ? <p>Cargando...</p> : <PlanetList planets={planets} />}
