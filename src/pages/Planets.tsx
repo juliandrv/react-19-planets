b94a073f19defc38220c1ca5522174ca6912b876
Fernando Herrera <fernando.herrera85@gmail.com>
2024-12-09 12:35:20 -0500

    Add fade-in animation and improve loading state in Planets component

--- src/pages/Planets.tsx
+++ src/pages/Planets.tsx
@@ -1,14 +1,17 @@
 import { FC, useEffect, useState } from 'react';
-import { Planet } from '../interfaces/planet.interface';
 import { planetsApi } from '../api/planetsApi';
+import { Planet } from '../interfaces/planet.interface';
 import { EditPlanetForm } from './ui/EditPlanetForm';
+import { PlanetList } from './ui/PlanetList';
 
 const Planets: FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [planets, setPlanets] = useState<Planet[]>([]);
 
   useEffect(() => {
     planetsApi.get('/').then((res) => {
       setPlanets(res.data);
+      setIsLoading(false);
     });
   }, []);
 
@@ -20,15 +23,7 @@ const Planets: FC = () => {
       <EditPlanetForm />
 
       {/* Lista de planetas Grid*/}
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-        {planets.map((planet) => (
-          <div key={planet.id} className="p-4 bg-gray-100 rounded shadow">
-            <h2 className="text-xl font-semibold">{planet.name}</h2>
-            <p className="text-gray-700">{planet.type}</p>
-            <p className="text-gray-700">{planet.distanceFromSun}</p>
-          </div>
-        ))}
-      </div>
+      {isLoading ? <p>Cargando...</p> : <PlanetList planets={planets} />}
     </div>
   );
 };
